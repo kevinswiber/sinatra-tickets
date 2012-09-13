d02dd37fb0794244962452b73827e853f188ab14
Phil Harvey <philharveyx@gmail.com>
2012-09-13 13:52:20 -0400

    Add home page, dynamic urls

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'http://rubygems.org'
 gem 'sinatra'
 gem 'sinatra-activerecord'
 gem 'nokogiri'
+gem 'emk-sinatra-url-for'
 
 group :development, :test do
   gem 'sqlite3'
--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'sinatra/url_for'
 require './config/environments'
 
+# Home
 get '/' do
-  'Hello World'
+  content_type "application/vnd.org.restfest.2012.hackday+xml"
+  erb :index, :locals => {
+    :self_url => url_for("/", :full),
+    :tickets_url => url_for("/tickets", :full),
+    :users_url => url_for("/users", :full),
+    :changes_url => url_for("/changes", :full)
+  }
 end
 
 # Search tickets
