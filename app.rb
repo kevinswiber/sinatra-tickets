cff51fb8bb3953bd54ed583cce4757e5159bb383
Phil Harvey <philharveyx@gmail.com>
2012-09-13 12:22:19 -0400

    Add some basic endpoints

--- app.rb
+++ app.rb
@@ -5,3 +5,15 @@ require './config/environments'
 get '/' do
   'Hello World'
 end
+
+# Search tickets
+get '/tickets' do
+end
+
+# Search users
+get '/users' do
+end
+
+# Change log
+get '/changes' do
+end
